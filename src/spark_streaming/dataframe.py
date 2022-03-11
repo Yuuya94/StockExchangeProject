b2f257caa35bf6a52f3ab0298f7e73bbbc73c691
xinyandu <xinyan.du@imt-atlantique.net>
2022-03-11 16:33:17 +0100

    spark-kafka

---
+++ src/spark_streaming/dataframe.py
@@ -0,0 +1,19 @@
+from pyspark.sql.types import *
+from pyspark.sql.functions import split
+from kafka_to_spark_streaming import sdfIBM
+
+ActionSchema = StructType([
+    StructField("ActionId", ShortType()),
+    StructField("Symbol",StringType()),
+    StructField("LastRefreshed", TimestampType())])
+
+def parse_data_from_kafka_message(sdf, schema):
+    assert sdf.isStreaming == True, "DataFrame doesn't receive streaming data"
+    col = split(sdf['value'], ',') #split attributes to nested array in one Column
+    #now expand col to multiple top-level columns
+    for idx, field in enumerate(schema):
+        sdf = sdf.withColumn(field.name, col.getItem(idx).cast(field.dataType))
+        return sdf.select([field.name for field in schema])
+
+
+sdfIBM = parse_data_from_kafka_message(sdfIBM, ActionSchema)
--- src/spark_streaming/kafka_to_spark_streaming.py
+++ src/spark_streaming/kafka_to_spark_streaming.py
@@ -7,19 +7,49 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder \
     .appName("Spark Structured Streaming from Kafka") \
     .getOrCreate()
-sdfRides = spark \
+
+sdfIBM = spark \
     .readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "taxirides") \
-    .option("startingOffsets", "latest") \
-    .load() \
-    .selectExpr("CAST(value AS STRING)")
-sdfFares = spark \
-    .readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
-    .option("subscribe", "taxifares") \
-    .option("startingOffsets", "latest") \
-    .load() \
-    .selectExpr("CAST(value AS STRING)")
+    .option("subscribe", "IBM") \
+    .option("startingOffsets", "earliest") \
+    .option("endingOffsets", "latest")\
+    .load()\
+    .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+# sdfAAPL = spark \
+#     .readStream \
+#     .format("kafka") \
+#     .option("kafka.bootstrap.servers", "localhost:9092") \
+#     .option("subscribe", "AAPL") \
+#     .option("startingOffsets", "latest") \
+#     .load() \
+#     .selectExpr("CAST(value AS STRING)")
+#
+# sdfAMZN = spark \
+#     .readStream \
+#     .format("kafka") \
+#     .option("kafka.bootstrap.servers", "localhost:9092") \
+#     .option("subscribe", "AMZN") \
+#     .option("startingOffsets", "latest") \
+#     .load() \
+#     .selectExpr("CAST(value AS STRING)")
+#
+# sdfTSLA = spark \
+#     .readStream \
+#     .format("kafka") \
+#     .option("kafka.bootstrap.servers", "localhost:9092") \
+#     .option("subscribe", "TSLA") \
+#     .option("startingOffsets", "latest") \
+#     .load() \
+#     .selectExpr("CAST(value AS STRING)")
+#
+# sdfBABA = spark \
+#     .readStream \
+#     .format("kafka") \
+#     .option("kafka.bootstrap.servers", "localhost:9092") \
+#     .option("subscribe", "BABA") \
+#     .option("startingOffsets", "latest") \
+#     .load() \
+#     .selectExpr("CAST(value AS STRING)")
